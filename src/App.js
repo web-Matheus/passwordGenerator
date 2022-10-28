10eb2f75f6814989a2a9495d9bbea24f7f505ec1
web-Matheus <matheussantosjere02@gmail.com>
2022-10-28 15:29:22 -0300

    layout desenvolvido com sucesso

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import Button from './components/button/button';
+import Password from './components/passwordfield/password';
 
 function App() {
+  const [lenghtpassword, setLenghtpassword] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+      <Password/>
+     <section className='container'>
+
+      <div className='range-data'>
+        <label>Character Lenght</label>
+        <h2>{lenghtpassword}</h2>
+      </div>
+      <div className='range-container'>
+        <input type="range" min="0" max="15" value={lenghtpassword}
+        onChange={(e) => setLenghtpassword(e.target.value)}/>
+      </div>
+      
+      <form>
+        <div>
+          <input type="checkbox"/>
+          <label>Include Uppercase Letters</label>
+        </div>
+        <div>
+          <input type="checkbox"/>
+          <label>Include Lowercase Letters</label>
+        </div>
+        <div>
+          <input type="checkbox"/>
+          <label>Include Numbers</label>
+        </div>
+        <div>
+          <input type="checkbox"/>
+          <label>Include Symbols</label>
+        </div>
+      </form>
+        <Button/>
+     </section>
     </div>
   );
 }
