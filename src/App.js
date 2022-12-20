10b8842756525db7b7cbc9a35db29a21be481040
web-Matheus <matheussantosjere02@gmail.com>
2022-12-20 10:59:05 -0300

    pronto para deploy primeira versão

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import React, { useState } from 'react'
 import './App.css';
-import Button from './components/button/button';
+import Arrow from  './assets/seta.png'
 import Password from './components/passwordfield/password';
 
 function App() {
-  const [lenghtpassword, setLenghtpassword] = useState(0);
+  const [lenghtpassword, setLenghtpassword] = useState(4);
+  const [senha, setSenha] = useState('')
 
+    const gerar = () => {
+
+    const alfabetoUp = [ 'A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N','O', 'P','Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+    const alfabetoLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    
+    const simbolos = ['!','#','$',"<","&","*",'/',":",";","(",")","@","~","+","<",">","?","|","="];
+    
+    let password ='';
+    
+     // parâtetro é tamanho da senha do usuário
+    
+        // a senha como o usuário no definiu ainda começa com zero
+        // então enquanto a variável password for menor que o tamanho da senha que o usuário quer 
+        //vamos percorrer um loop
+        while(password.length <= lenghtpassword-1){
+    
+        
+            const GeradorDeLowerLetters = Math.floor(Math.random() * alfabetoLower.length); //sortear uma letra maiúscula no meio do array
+    
+            const randomNumber  = Math.floor( Math.random() * 10) //criar números de 0 a 9;
+    
+            const randomUpLetters = Math.floor( Math.random() * alfabetoUp.length) //sortear uma letra minúscula ...
+    
+            const randomSimbolos = Math.floor( Math.random() * simbolos.length) //sortear um simbolo
+    
+            //enquanto o loop for verdade acrescenta mais um caracterie para password
+            password += `${alfabetoLower[GeradorDeLowerLetters]}${simbolos[randomSimbolos]}${alfabetoUp[randomUpLetters]}${randomNumber}`;
+        
+            setSenha(password)
+        }
+    }
   return (
     <div>
-     
-      <Password/>
+
+      <Password senha={senha}/>
      <section className='container'>
 
       <div className='range-data'>
@@ -17,7 +50,7 @@ function App() {
         <h2>{lenghtpassword}</h2>
       </div>
       <div className='range-container'>
-        <input type="range" min="0" max="15" value={lenghtpassword}
+        <input type="range" min="4" max="15" value={lenghtpassword}
         onChange={(e) => setLenghtpassword(e.target.value)}/>
       </div>
       
@@ -39,8 +72,11 @@ function App() {
           <label>Include Symbols</label>
         </div>
       </form>
-        <Button 
-        lenghtpassword={lenghtpassword}/>
+      <div className='button'>
+        <button  onClick={gerar}
+          >Generate <img src={Arrow}
+          alt="seta icone"/></button>
+    </div>
      </section>
     </div>
   );
--- src/components/passwordfield/password.js
+++ src/components/passwordfield/password.js
@@ -1,16 +1,21 @@
 import React from 'react'
-
+import copyImage from '../../assets/copy.png'
 import './password.css'
-const Password = () => {
-  const Copie = () =>{
-    var label = document.getElementById("label")
-  
-
-  }
+const Password = ({senha}) => {
+    const copy =()=>{
+      var copyText = document.getElementById("input")
+      copyText.select();
+      copyText.setSelectionRange(0, 99999); // For mobile devices
+    
+       // Copy the text inside the text field
+      navigator.clipboard.writeText(copyText.value);
+      alert("Copied the text: " + copyText.value);
+    }
   return (
-    <div className='passwordfield' id='label'>
-        <label onClick={Copie()}
-        >P3x#2P0o</label>       
+    <div className='passwordfield' >
+        <input type="text" value={senha} id="input"/>  
+        <img src={copyImage} alt="icone para copiar a senha gerada"
+      onClick={copy}/>
     </div>
   )
 }
